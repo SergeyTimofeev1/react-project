d9df42157c1515c2d8d0ba6669a22f95012371f8
SergeyTimofeev1 <timofeevbest@yandex.ru>
2022-11-27 17:14:39 +0400

    feat: add modal

--- src/App.js
+++ src/App.js
@@ -1,34 +1,58 @@
-import { useRef, useState } from "react";
+import { useMemo, useRef, useState } from "react";
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
 import './styles/app.css'
 import MyButton from './components/UI/button/MyButton';
 import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
+import MySelect from './components/UI/select/MySelect';
+import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/modal/MyModal";
 
 
 function App() {
 
   const [posts, setPosts] = useState([
-    {id: 1, title: 'Js', body: 'Lorem ipsum dolor sit amet.'},
-    {id: 2, title: 'Ts', body: 'Lorem ipsum dolor sit amet.'},
-    {id: 3, title: 'Css', body: 'Lorem ipsum dolor sit amet.'},
+    {id: 1, title: 'Js', body: 'A.Lorem ipsum dolor sit amet.'},
+    {id: 2, title: 'Ts', body: 'B.Lorem ipsum dolor sit amet.'},
+    {id: 3, title: 'Css', body: 'C.Lorem ipsum dolor sit amet.'},
   ]);
-  
+
+  const [filter, setFilter] = useState({sort: '', query:''})
+  const [modal, setModal] = useState(false)
+
+  const sortedPosts = useMemo(() => {
+    if(filter.sort) {
+      return [...posts].sort((a,b) => a[filter.sort].localeCompare(b[filter.sort]))
+    }
+    return posts
+  }, [filter.sort, posts])
+
+  const sortedAndSearchedPosts = useMemo(()=> {
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
+  }, [filter.query, sortedPosts])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
+    setModal(false)
   }
 
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
-    
+
   return (
     <div className="App">
-      <PostForm create={createPost}/>
-      <PostList remove={removePost} posts={posts} title={'post list 1'}/>
+      <MyButton onClick={() => setModal(true)}>Создать пост</MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost}/>
+      </MyModal>
+      <PostFilter
+        filter={filter}
+        setFilter={setFilter}
+      />
+      <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'post list 1'}/>
      </div>
   );
 }
