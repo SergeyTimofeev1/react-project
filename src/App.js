8bb4bab407d4f5e18f1658be78b6041cba01ebf3
SergeyTimofeev1 <timofeevbest@yandex.ru>
2022-11-27 17:23:20 +0400

    feat: add custom hooks

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import PostForm from "./components/PostForm";
 import MySelect from './components/UI/select/MySelect';
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/modal/MyModal";
+import { usePosts } from "./hooks/usePosts";
 
 
 function App() {
@@ -20,17 +21,7 @@ function App() {
 
   const [filter, setFilter] = useState({sort: '', query:''})
   const [modal, setModal] = useState(false)
-
-  const sortedPosts = useMemo(() => {
-    if(filter.sort) {
-      return [...posts].sort((a,b) => a[filter.sort].localeCompare(b[filter.sort]))
-    }
-    return posts
-  }, [filter.sort, posts])
-
-  const sortedAndSearchedPosts = useMemo(()=> {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-  }, [filter.query, sortedPosts])
+  const sortedAndSearchedPosts = usePosts(posts,filter.sort,filter.query)
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
